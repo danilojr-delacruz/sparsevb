ba4476d298b82e6ebeea234e7a20207816244605
Danilo Jr Dela Cruz <djd19@ic.ac.uk>
2022-07-26 01:29:19 +0100

    Implement NumericIntCauchy.

--- sparsevb/vb.py
+++ sparsevb/vb.py
@@ -297,10 +297,68 @@ class JensenBoundCauchy(BaseVB):
             np.log(self.a0 / self.b0),
             -self.mu_function(i, mu, sigma, gamma)(mu[i]),
             -self.XX[i, i] * (sigma[i]**2) / 2,
-            (1 + np.log(2*np.pi) )/ 2
+            (1 + np.log(2*np.pi) )/ 2,
+            -np.log(sigma[i])
         ]
         return sum(terms)
 
 
     def __repr__(self):
         return "JensenBoundCauchy()"
+
+
+class NumericIntCauchy(BaseVB):
+
+    @staticmethod
+    @np.vectorize
+    def neg_expected_log_g(mu, sigma):
+        def A(t):
+            term1 = np.log(1 + (t+mu)**2)
+            term2 = np.exp(-(t/sigma)**2 / 2) / (sigma * np.sqrt(2*np.pi))
+            return term1*term2
+        
+        def B(t):
+            return A(1/t) / t**2
+
+        left = sp.integrate.quad(B, -1, 0)[0]
+        middle = sp.integrate.quad(A, -1, 1)[0]
+        right = sp.integrate.quad(B, 0, 1)[0]
+
+        return left + middle + right + np.log(np.pi)
+
+    def mu_function(self, i, mu, sigma, gamma):
+        mask = (np.arange(self.p) != i)
+        def func(mu_i):
+            terms = [
+                mu_i * (self.XX[i, :] * gamma * mu)[mask].sum(),
+                self.XX[i, i] * (mu_i**2) / 2,
+                - self.YX[i] * mu_i,
+                self.neg_expected_log_g(mu_i, sigma[i])
+            ]
+            return sum(terms)
+        return func
+
+    def sigma_function(self, i, mu, sigma, gamma):
+        def func(sigma_i):
+            terms = [
+                self.XX[i, i] * (sigma_i**2) / 2,
+                -np.log(sigma_i),
+                self.neg_expected_log_g(mu[i], sigma_i)
+            ]
+            return sum(terms)
+        return func
+
+    def gamma_function(self, i, mu, sigma, gamma):
+        terms = [
+            np.log(self.a0 / self.b0),
+            -self.mu_function(i, mu, sigma, gamma)(mu[i]),
+            -self.XX[i, i] * (sigma[i]**2) / 2,
+            (1 + np.log(2*np.pi) )/ 2,
+            -np.log(sigma[i])
+        ]
+        return sum(terms)
+
+
+    def __repr__(self):
+        return "JensenBoundCauchy()"
+
